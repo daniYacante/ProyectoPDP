cef159823e9c3845f9d24b6730eec28ddc2e83e9
daniYacante <danizyacante@gmail.com>
2023-08-25 17:17:12 -0300

    Tratando de arreglar moco

--- src/Core/Game.java
+++ src/Core/Game.java
@@ -3,8 +3,7 @@ package Core;
 public class Game {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Bienvenido a Calabozos y Profesores");
 	}
 
 }
